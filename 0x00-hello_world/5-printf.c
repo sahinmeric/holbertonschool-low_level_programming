b1036e82f99cf3810c94cb1dccc44bc3ce218039
Sahin <sahin.meric86@gmail.com>
2021-09-30 10:33:20 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Short description, single line
+ *
+ * Description: Longer description of the function)?
+ * (* section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is piece of art,\n");
+
+return (0);
+}

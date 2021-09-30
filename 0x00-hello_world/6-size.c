29bc302655c4d7dd6b35f70f89a1ca9dc3686c81
Sahin <sahin.meric86@gmail.com>
2021-09-30 11:00:25 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Short description, single line
+ *
+ * Description: Longer description of the function)?
+ * (* section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int))
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}

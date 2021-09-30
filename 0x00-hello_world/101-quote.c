4b5608da68d8cdddf23248e445480a7e5de62aa0
Sahin <sahin.meric86@gmail.com>
2021-09-30 16:48:30 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Short description, single line
+ *
+ * Description: Longer description of the function)?
+ * (* section header: Section description)*
+ * Return: Description of the returned value
+ */
+
+int main()
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return(0);
+}

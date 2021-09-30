684034ca255159b6fee1a401923e87d30a9d53a8
Sahin <sahin.meric86@gmail.com>
2021-09-30 10:24:12 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+/**
+ * main - Short description, single line
+ *
+ * Description: Longer description of the function)?
+ * (* section header: Section description)*
+ * Return: Description of the returned value
+ */
+
 int main(void)
 {
+
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }

e3713a0c40222c48754cb01256929acb67d3794d
Sahin <sahin.meric86@gmail.com>
2021-10-01 10:11:59 -0700

    0x01 created

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,19 @@
  * (* section header: Section description)*
  * Return: Description of the returned value
  */
+int sum();
 
 int main(void)
 {
+	int addition;
+	addition=sum();
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	printf("%d",addition);
 	return (0);
+}
+int sum()
+{
+	int a=1,b=2,sum;
+	sum = a + b;
+	return(sum);
 }

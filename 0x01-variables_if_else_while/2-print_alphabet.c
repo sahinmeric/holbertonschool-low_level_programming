f2dddcae4a60f142c1166c9de096e64e3754bfe2
Sahin <sahin.meric86@gmail.com>
2021-10-01 12:20:26 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Description: Generate random number and assign it to a variable
+ *
+ * Return: Returns 0 success
+ */
+
+int main(void)
+{
+	int x;
+	for(x = 'a'; x <= 'z'; x++)
+		{
+		x = tolower(x);
+		putchar(x);
+		putchar('\n');
+					        }
+	return (0);
+}

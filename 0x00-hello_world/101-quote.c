d1c5da45dd9522cec20118aaf140e5b3306eced0
Sahin <sahin.meric86@gmail.com>
2021-10-02 16:35:22 -0700

    char used

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -14,6 +14,8 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	perror(message);
+
 	return (1);
 }

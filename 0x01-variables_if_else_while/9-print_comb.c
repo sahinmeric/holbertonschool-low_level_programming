96eebadb6759f38c57c7e1eba226cfd2ae59ee75
Sahin <sahin.meric86@gmail.com>
2021-10-01 18:40:24 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Description: Generate random number and assign it to a variable
+ *
+ * Return: Returns 0 success
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	if ( n < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}

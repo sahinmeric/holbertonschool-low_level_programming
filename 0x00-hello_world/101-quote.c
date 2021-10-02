0eb9ad1c8dee3cfb9f1f441c7774359afb06bc58
Sahin <sahin.meric86@gmail.com>
2021-10-02 15:59:22 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 /**
- * main - Short description, single line
+ * main - program that prints a quote with a new line, to the standard error.
  *
  * Description: Longer description of the function)?
  * (* section header: Section description)*
  * Return: Description of the returned value
  */
-int sum();
 
 int main(void)
 {
-	int addition;
-	addition=sum();
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	printf("%d",addition);
 	return (0);
-}
-int sum()
-{
-	int a=1,b=2,sum;
-	sum = a + b;
-	return(sum);
 }

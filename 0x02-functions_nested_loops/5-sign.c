884dc539448956fed85de97fdaeba5301cd7cb20
Sahin <sahin.meric86@gmail.com>
2021-10-05 12:47:24 -0700

    corrected

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,12 +12,12 @@ int print_sign(int n)
 	if (n > 48)
 	{
 	return (1);
-	_putchar('+');
+	_putchar(43);
 	}
 	else if (n < 48)
 	{
 	return (-1);
-	_putchar('-');
+	_putchar(45);
 	}
 	else
 	_putchar(48);

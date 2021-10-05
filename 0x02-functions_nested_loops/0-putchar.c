e6bf0c1e4dedeba7ccfec0e791daeebd7a4d7a59
Sahin <sahin.meric86@gmail.com>
2021-10-05 09:43:11 -0700

    used muchos putchars

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,6 +11,13 @@
 
 int main(void)
 {
-	_putchar('c');
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
 	return (0);
 }

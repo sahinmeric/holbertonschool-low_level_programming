f66fb48cb47d56e35b74e60e9f56afbb5eddbffd
Sahin <sahin.meric86@gmail.com>
2021-10-05 12:44:33 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ *
+ * Description: function that prints the sign of a number.
+ * @n: argument that we will check the value
+ * Return: returns 1 if greater than zero or -1 if less than zero, 0 for zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+	return (1);
+	_putchar('+');
+	}
+	else if (n < 48)
+	{
+	return (-1);
+	_putchar('-');
+	}
+	else
+	_putchar(48);
+	return (0);
+}

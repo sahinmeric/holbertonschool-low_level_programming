6fec95df229d6369721abf72105a5d240d458994
Sahin <sahin.meric86@gmail.com>
2021-10-06 08:22:34 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,16 +13,27 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int k;
 
 	for (i = 0; i < 10; i++)
 	{
+		_putchar('0');
+
 		for (j = 0; j < 10; j++)
+			_putchar(',');
+			_putchar(' ');
+		k = i * j;
+
+		if (k < 10)
 		{
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(j + '0');
-			_putchar(',');
-			_putchar(i * j + '0');
+			_putchar(' ');
+		} else
+		{
+			_putchar((k / 10) + '0');
 		}
-	}
+		_putchar((k % 10) + '0');
+		}
+		_putchar('\n');
 }
+
+

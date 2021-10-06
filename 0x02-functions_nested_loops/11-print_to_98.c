2b46615c6ef31fd6f7197da47b261b9325d38712
Sahin <sahin.meric86@gmail.com>
2021-10-06 16:33:48 -0700

    i know its not right

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,10 +12,10 @@
 
 void print_to_98(int n)
 {
-	int i;
-
 	if (n < 99)
 	{
+	int i;
+
 	for (i = n; i < 99; i++)
 	{
 		if (n != 98)
@@ -30,6 +30,8 @@ void print_to_98(int n)
 	}
 	else
 	{
+	int i;
+
 	for (i = n; i > 97; i--)
 	{
 		if (n != 98)

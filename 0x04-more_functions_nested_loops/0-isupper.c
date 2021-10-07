1156bb531c149ab14d8e2a54ff72ba8cb9e8c2f6
Sahin <sahin.meric86@gmail.com>
2021-10-07 11:35:49 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isupper - returns the largest of 3 numbers
+ * @c: the value that will me checked for uppercase
+ * Return: largest number
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+	_putchar('1');
+	return (1);
+	}
+	else
+	return (0);
+}

e1c54fb9c913be8f214e391db58cb2404a254381
Sahin <sahin.meric86@gmail.com>
2021-10-07 11:58:47 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * mul - returns the largest of 3 numbers
+ * @a: first value for the multiply
+ * @b: second value for the multiply
+ * Return: result of multiplacation
+ */
+
+int mul(int a, int b)
+{
+	int res = a * b;
+
+	return (res);
+}

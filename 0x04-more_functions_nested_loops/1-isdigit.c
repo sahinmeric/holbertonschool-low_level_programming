75460f5a9ad575f408aca6c75df10c038bd704ff
Sahin <sahin.meric86@gmail.com>
2021-10-07 11:54:50 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * _isdigit - returns the largest of 3 numbers
+ * @c: the value that will me checked for uppercase
+ * Return: largest number
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+	return (1);
+	}
+	else
+	return (0);
+}

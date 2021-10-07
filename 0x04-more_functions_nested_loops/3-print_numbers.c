b57d2e1790ae852b9b9745438a951517cc4d3ff4
Sahin <sahin.meric86@gmail.com>
2021-10-07 12:03:09 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * print_numbers - returns the largest of 3 numbers
+ *
+ * Return: result of multiplacation
+ */
+
+int print_numbers()
+{
+	int i = 0;
+	
+	while(i < 10)
+	{
+	_putchar(i + '0');
+	}
+	return(0)
+}

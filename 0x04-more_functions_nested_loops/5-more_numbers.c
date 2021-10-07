1ed43fafe64dd1c3d6f75c76d4e7c8ed77062755
Sahin <sahin.meric86@gmail.com>
2021-10-07 16:34:25 -0700

    corrected

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,21 +10,19 @@
 void more_numbers(void)
 {
 	int j;
-
-	for (j = 0; j <= 10; j++)
-	{
-
 	int i;
 
-	for (i = 0; i < 14; i++)
+	for (j = 0; j < 10; j++)
 	{
+
+	for (i = 0; i <= 14; i++)
+		{
 		if (i > 9)
 		{
-		_putchar((i / 10) + '0');
+		_putchar((i / 10) + 48);
 		}
-		else
 		_putchar((i % 10) + '0');
 		}
+		_putchar(10);
 	}
-		_putchar('\n');
 }

a0f2245ae5bfb6716c576a617f14fe897617d87f
Sahin <sahin.meric86@gmail.com>
2021-10-12 11:06:29 -0700

    first try strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - swaps the values of two integers.
+ * @s: This is the string whose length is to be found.
+ *
+ * Return: 0 success
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}

d2d56a1b7aeed681135e18817f694d76d9323256
Sahin <sahin.meric86@gmail.com>
2021-10-13 09:54:25 -0700

    print array items

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,9 +15,16 @@ void print_array(int *a, int n)
 	for (i = 0; i < n ; i++)
 	{
 		printf("%d", a[i]);
-
+		
+		if ( i != n-1)
+		{
 		putchar(',');
 		putchar(' ');
+		}
+		else
+		{
+		putchar(' ');
+		}
 	}
 		putchar('\n');
 }

83c03b75e1222cf11f69c8fdc5551e93802b406b
Sahin <sahin.meric86@gmail.com>
2021-10-20 13:48:48 -0700

    length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,9 +9,16 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (s == NULL)
+	int len = 0;
+
+	if (s != NULL)
 	{
-		return (0);
+		len++;
+		_strlen_recursion(++s);
 	}
-	return (1 + _strlen_recursion(s + 1));
+	else
+	{
+	printf("%d", len);
+	}
+	return (0);
 }

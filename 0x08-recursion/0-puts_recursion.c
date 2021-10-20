11ee97767cfd000087c6822a529eb326b27e238d
Sahin <sahin.meric86@gmail.com>
2021-10-20 13:11:46 -0700

    put recursivo

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line.
+ *
+ *@s: string will be printed
+ * Return: 0 success
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s", s);
+}

648ad2c979194675cd4fa4159a540f2127d3549a
Sahin <sahin.meric86@gmail.com>
2021-10-22 16:25:35 -0700

    addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,10 +10,10 @@
   */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, j;
 	int res = 0;
 
-	if (argc == 0)
+	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
@@ -22,16 +22,22 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		/*  check if there is a symbol or character in the given numbers
-		 *  if there is print Error\n and return 1 */
-	}
+		 *  if there is print Error\n and return 1
+		 **/
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 58 || argv[i][j] <= 47)
+				{
+					printf("Error\n");
+					return (1);
+				}
+		}
 
 		/* add the given numbers and return result */
 
-		for (i = 1; i < argc; i++)
-		{
-			res = res + atoi(argv[i]);
-		}
+		res = res + atoi(argv[i]);
+	}
 		printf("%d\n", res);
 
-	return (0);
+		return (0);
 }

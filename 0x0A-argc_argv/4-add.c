f50689e5477aa9468fdaee1ae95153885e0bfb74
Sahin <sahin.meric86@gmail.com>
2021-10-22 16:35:38 -0700

    fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] < 58 || argv[i][j] >= 47)
+			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{
 				printf("Error\n");
 				return (1);

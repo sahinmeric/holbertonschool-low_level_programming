b31fd826a3d44ca7ebb88a0c2484922b5027c134
Sahin <sahin.meric86@gmail.com>
2021-10-22 10:11:51 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,18 +8,21 @@
   *@argv: array of pointers to the strings which are those arguments
   * Return: exit status, 0 is success, other than 0 is error
   */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
+	if (argc != 2)
+	{
+		printf("Error");
+		return (-1);
+	}
+	else
+	{
 	int i = atoi(argv[1]);
 	int j = atoi(argv[2]);
 	int multiply = i * j;
 
-	if (argc == 3)
-	{
 	printf("%d\n", multiply);
 	}
-	else
-		printf("Error");
 
 	return (0);
 }

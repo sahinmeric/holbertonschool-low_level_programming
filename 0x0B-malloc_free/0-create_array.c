4ed74cce1f9bfe27d539470b19712eff68ab19d9
Sahin <sahin.meric86@gmail.com>
2021-10-26 12:30:31 -0700

    for loop added

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,8 +11,11 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array = (char *)malloc(size * sizeof(char));
+	unsigned int i;
 
-	array[0] = c;
-
+	for (i = 0 ; i < size ; i++)
+	{
+	array[i] = c;
+	}
 	return (array);
 }

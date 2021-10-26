16e8678e21ade1a3244e5b82a9366008d505c514
Sahin <sahin.meric86@gmail.com>
2021-10-26 12:34:41 -0700

    return null added

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,11 @@ char *create_array(unsigned int size, char c)
 	char *array = (char *)malloc(size * sizeof(char));
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
 	for (i = 0 ; i < size ; i++)
 	{
 	array[i] = c;

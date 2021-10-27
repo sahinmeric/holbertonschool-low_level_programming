15f7b79b3e35aa43aee14b3d6c8d0da69a7add6d
Sahin <sahin.meric86@gmail.com>
2021-10-27 15:13:36 -0700

    free added

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,6 +25,14 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 	grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+		for (i = 0; i < width; i++)
+		{
+		free(grid[i]);
+		}
+		free(grid);
+		}
 	}
 	return (grid);
 }

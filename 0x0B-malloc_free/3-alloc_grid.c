01dc531c3f6f64753b9beebd2a78f15b633d6c49
Sahin <sahin.meric86@gmail.com>
2021-10-27 08:36:17 -0700

    another try

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,9 +21,13 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < width; i++)
 	{
+	grid[i] = malloc(sizeof(int) * width);
+	}
+	/* for (i = 0; i < width; i++)
+	{
 		for (j = 0; j < height; j++)
 			grid[i][j] = 0;
-	}
+	} */
 
 	return (grid);
 

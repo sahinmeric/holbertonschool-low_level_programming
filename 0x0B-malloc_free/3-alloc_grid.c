ba0f59905cc710bdf78f06df539f899777b3de40
Sahin <sahin.meric86@gmail.com>
2021-10-27 08:49:23 -0700

    <=0

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 	int **grid;
 	int i;
 
-	if (width == 0 || height == 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 	grid = malloc(sizeof(int *) * height);
 

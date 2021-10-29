0d29370d52fae1612386eeed46308852e9074e57
Sahin <sahin.meric86@gmail.com>
2021-10-28 19:00:12 -0700

    start files created

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *malloc_checked - function that allocates memory using malloc
+ *
+ *@b: amount of bytes that will be allocated
+ * Return: On success 0, 98 on fail
+ */
+void *malloc_checked(unsigned int b)
+{
+
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void *malloc_checked(unsigned int b);
+#endif /* MAIN_H */

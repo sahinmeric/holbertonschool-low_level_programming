afd914d5a90426b3d58eb209819a607855938a2f
Sahin <sahin.meric86@gmail.com>
2021-11-01 10:46:52 -0700

    first try

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - function that allocates memory for an array, using malloc.
+ *
+ * Return: On success 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}

f82633cbab1206eac7a7f1779dbe455efc0a9d71
Sahin <sahin.meric86@gmail.com>
2021-11-01 17:01:52 -0700

    betty fix2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+/**
+ * struct dog - structing a type of dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner name
+ * Description: Longer description
+ */
 struct dog
 {
 char *name;

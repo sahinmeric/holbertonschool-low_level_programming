e0ba5074e7076139ed6104bf4342c2d812aae1a6
Sahin <sahin.meric86@gmail.com>
2021-11-01 19:40:12 -0700

    age corrected

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,12 +18,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		}
 
-		if (d->age != 0)
 			printf("Age: %f\n", d->age);
-		else
-		{
-			printf("Age: (nil)\n");
-		}
 
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);

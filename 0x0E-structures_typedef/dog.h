4eb43987a221456871465e36640c4ca93845c0d0
Sahin <sahin.meric86@gmail.com>
2021-11-01 17:45:26 -0700

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,7 @@ float age;
 char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 
 #endif /* MAIN_H */
--- 0x0E-structures_typedef/main.h
+++
@@ -1,5 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-void init_dog(struct dog *d, char *name, float age, char *owner);
-#endif /* MAIN_H */

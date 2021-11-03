b841ad4282e60f7acc4997232f26c3959cb438e0
Sahin <sahin.meric86@gmail.com>
2021-11-03 09:48:23 -0700

    actualizar

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ *print_dog - function that prints a struct dog
+ *
+ *@d: argument
+ * Return: void
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+
+			printf("Age: %f\n", d->age);
+
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
+
+	}
+}

13612674672d1945437eeb0acc67e499f3e751fb
Sahin <sahin.meric86@gmail.com>
2021-11-03 13:44:06 -0700

    another try

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,6 +15,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
+
 	if (array != NULL && cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
@@ -33,5 +34,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 	else
 		return (-1);
 
-	return (i);
+	return (-1);
 }

83b0406ca53a99785a9577aec836fdc0a4614bf8
Sahin <sahin.meric86@gmail.com>
2021-11-03 13:15:24 -0700

    another try

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,9 +13,22 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (size <= 0)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
-		cmp(array[i]);
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+
+		if (cmp(array[i] == 0))
+		{
+			return (-1);
+		}
+
 	}
-	return (0);
+
+	return (i);
 }

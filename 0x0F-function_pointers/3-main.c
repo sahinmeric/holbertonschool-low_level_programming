3276e859a49efbc87610682aa7c7c1a1535106ac
Sahin <sahin.meric86@gmail.com>
2021-11-03 19:52:30 -0700

    push

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "3-calc.h"
 /**
  *main -  entry point
  *
- *
+ *@argc: number of arguments
+ *@argv: arguments
  * Return: returns a pointer to the function
  * that corresponds to the operator given as a parameter.
  */
 int main(int argc, char *argv[])
 {
-	get_op_func(argv[2]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	else
+
 }
+

312f4240065f1c807f95e21dd058604051a91f35
Sahin <sahin.meric86@gmail.com>
2021-11-05 14:24:59 -0700

    fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,48 +1,48 @@
-#include <stdio.h>
-#include <stddef.h>
 #include "variadic_functions.h"
+#include <stddef.h>
+#include <stdio.h>
 
 /**
- *print_all - function that prints anything
- * @format: seperator of integers passed to func
- * Return: Return void
+ * print_all - print parameters
+ * @format: kind of argument
+ * Return: Always 0.
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0, len = 0;
-	char *str, *separator = ", ";
-	va_list args;
+	int type = 0, counter = 0;
+	char *auxs, *separator = ", ";
+	va_list kind_of;
 
-	va_start(args, format);
-	while (format[len])
-		len++;
-	while (format && format[i])
+	va_start(kind_of, format);
+	while (format && format[counter])
+		counter++;
+	while (format && format[type])
 	{
-		if (i == len - 1)
+		if (type == counter - 1)
 			separator = "";
-		switch (format[i])
+		switch (format[type])
 		{
 			case ('c'):
-				printf("%c%s", va_arg(args, int), separator);
+				printf("%c%s", va_arg(kind_of, int), separator);
 			break;
 			case ('i'):
-				printf("%d%s", va_arg(args, int), separator);
+				printf("%d%s", va_arg(kind_of, int), separator);
 			break;
 			case ('f'):
-				printf("%f%s", va_arg(args, double), separator);
+				printf("%f%s", va_arg(kind_of, double), separator);
 			break;
 			case ('s'):
-				str = va_arg(args, char *);
-				if (str == NULL)
+				auxs = va_arg(kind_of, char *);
+				if (auxs == NULL)
 				{
 					printf("(nil)%s", separator);
 					break;
 				}
-				printf("%s%s", str, separator);
+				printf("%s%s", auxs, separator);
 			break;
 		}
-		i++;
+		type++;
 	}
 	printf("\n");
-	va_end(args);
+	va_end(kind_of);
 }

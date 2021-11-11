b84f4dae7011acde3affaba6171858dcd69a27f5
Sahin <sahin.meric86@gmail.com>
2021-11-11 14:02:53 -0800

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,11 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
 		}
 	h = h->next;
 	i++;

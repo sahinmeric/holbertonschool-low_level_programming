17cf18e1e3c3b75151578304806ad82a3e93007e
Sahin <sahin.meric86@gmail.com>
2021-11-22 17:44:22 -0800

    another try

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!buffer)
 		return (0);
-	fd = open(filename, O_RDWR, 0600);
+	fd = open(filename, O_RDONLY, 0600);
 
 	if (fd == -1)
 		return (0);

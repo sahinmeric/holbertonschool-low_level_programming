4e26e767d711732930f204d72c6994c56d94d9a6
Sahin <sahin.meric86@gmail.com>
2022-01-09 12:23:01 -0800

    fix

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,7 +7,7 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *tmp;
+	dlistint_t *new, *tmp = *head;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -21,7 +21,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = new;
 	else
 	{
-		while(tmp->next != NULL)
+		while (tmp->next != NULL)
 			tmp = tmp->next;
 		new->prev = tmp;
 		new->next = NULL;

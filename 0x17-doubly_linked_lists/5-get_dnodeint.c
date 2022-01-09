86a98b4f7b9e7b482152c072fe2a0a3c4a7e9125
Sahin <sahin.meric86@gmail.com>
2022-01-09 14:58:16 -0800

    first try

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function that returns thenth node
+ * @head: head pointer of double linked list
+ * @index: index
+ * Return: void
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *tmp = NULL;
+	ssize_t i;
+
+	for (i = 0; i < index; i++)
+	{
+		tmp = head;
+		head = head->next;
+	}
+	return (tmp);
+}

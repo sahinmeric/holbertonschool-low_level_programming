d94490e47cc960041a830a5fd3590f7605072c5e
sahinmeric <sahin.meric86@gmail.com>
2022-01-28 10:37:24 -0300

    first commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table with an array of pointers that points a linked list
+ * @size : size of array
+ * return : returns pointer to newly created hash table, or NULL in case error 
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *new = NULL;
+
+    if (size == 0)
+        return NULL;
+    new = malloc(sizeof(hash_table_t));
+    if (new == NULL)
+    {
+        return NULL;
+    }
+    new->size = size;
+    new->array = malloc(sizeof(hash_table_t*) * size);
+    if (new->array == NULL)
+    {
+        free(new);
+        return NULL;
+    }
+    return new;
+}
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+/**
+ * key_index - function that gives you the index of a key.
+ * @size : is the size of the array of the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    return (1);
+}

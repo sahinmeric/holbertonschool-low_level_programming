612d78d8b5359354fccbf3c412ec614cd781a775
Sahin <sahin.meric86@gmail.com>
2022-01-28 06:10:19 -0800

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	new->size = size;
-	new->array = malloc(sizeof(*hash_table_t) * size);
+	new->array = malloc(sizeof(hash_table_t *) * size);
 	if (new->array == NULL)
 	{
 		free(new);

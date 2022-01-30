1f8c7998909327fc7db4b4579f53e0e496489589
sahinmeric <sahin.meric86@gmail.com>
2022-01-30 20:35:00 -0300

    first try

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set- function that adds an element to the hash table.
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ * Return: 1 if success, else 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	unsigned long int index;
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = NULL;
+
+	ht->array[index] = new;
+	return (1);
+}

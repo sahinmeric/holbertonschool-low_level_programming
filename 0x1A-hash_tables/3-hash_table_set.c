aef11e498f55576121981121bd32e1a1abe06b13
sahinmeric <sahin.meric86@gmail.com>
2022-01-30 20:37:30 -0300

    some border cases added

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,6 +12,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new;
 	unsigned long int index;
 
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0 || value == NULL)
+		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 
 	new = malloc(sizeof(hash_node_t));

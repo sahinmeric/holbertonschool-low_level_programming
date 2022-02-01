a7160414d537c87d24b5f0491cc8852c8892b7ce
sahinmeric <sahin.meric86@gmail.com>
2022-02-01 11:18:32 -0300

    betty fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,12 +15,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || strcmp(key, "") == 0 || value == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-		return (0);
-    tmp = ht->array[index];
-    while (tmp != NULL)
+	tmp = ht->array[index];
+	while (tmp != NULL)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
@@ -30,6 +26,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		tmp = tmp->next;
 	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = NULL;
